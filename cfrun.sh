94b9bfa0f7ac7d01fbb40b2d48c820c9e8f3e169
zhu327 <zhu327@gmail.com>
2020-12-19 16:30:50 +0800

    add cf

--- cfrun.sh
+++ cfrun.sh
@@ -2,4 +2,4 @@ pod_name=$(kubectl get pods|awk 'END{print $1}')
 
 domain=$(kubectl logs $pod_name -c tunnel|grep trycloudflare.com|awk 'END{print $3}'|awk -F/ '{print $3}')
 
-sed -i "s/v2ray_host/$domain/g" index.js
+sed -i "s/v2ray_host/$domain/g" ./cf/index.js
