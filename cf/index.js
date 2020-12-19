694aa04691d1b4f9e80ef482c487cde5558e8793
zhu327 <zhu327@gmail.com>
2020-12-19 16:55:47 +0800

    mod cf

--- cf/index.js
+++ cf/index.js
@@ -1,7 +1,7 @@
 addEventListener(
   "fetch", event => {
     let url = new URL(event.request.url);
-    url.host = "loads-editions-handed-relate.trycloudflare.com";
+    url.host = "v2ray_host";
     let request = new Request(url, event.request);
     event.respondWith(
       fetch(request)
