82f09ab4f24a7c35817f3daf5dd4787e6c20543e
zhu327 <zhu327@gmail.com>
2020-12-28 22:51:25 +0800

    remove cf worker

--- subscribe.sh
+++ subscribe.sh
@@ -2,11 +2,9 @@ pod_name=$(kubectl get pods|awk 'END{print $1}')
 
 domain=$(kubectl logs $pod_name -c tunnel|grep trycloudflare.com|awk 'END{print $3}'|awk -F/ '{print $3}')
 
-sed -i "s/v2ray_host/$domain/g" ./cf/index.js
-
 ip=$(ibmcloud ks worker ls --cluster $IKS_CLUSTER|awk 'NR==3{print $2}')
 
-echo -e "$ip\n$domain" > ip.txt
+echo -e "$ip\n$domain" > mail.txt
 
 sed -i "s/v2ray_ip/$ip/g" subscribe.py
 sed -i "s/v2ray_host/$domain/g" subscribe.py
